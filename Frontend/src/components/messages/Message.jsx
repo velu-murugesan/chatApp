2fdbc7fe8cbeb85e3ee97626ee1de488bc95a507
velu-murugesan <velu2092002@gmail.com>
2024-04-19 21:41:40 +0530

    design part is completd in frontend

---
+++ Frontend/src/components/messages/Message.jsx
@@ -0,0 +1,21 @@
+const Message = () => {
+  return (
+    <>
+      <div className='chat chat-start'>
+        <div className='chat-image avatar'>
+          <div className='w-10 rounded-full'>
+            <img
+              alt='Tailwind CSS chat bubble component'
+              src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+            />
+          </div>
+        </div>
+        <div className='chat-bubble'>
+          It was said that you would, destroy the Sith, not join them.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Message;
