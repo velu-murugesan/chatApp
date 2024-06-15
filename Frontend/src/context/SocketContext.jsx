7d55e55e0175d83e1041256d946fc37aad36ce7c
velu-murugesan <velu2092002@gmail.com>
2024-06-15 09:59:22 +0530

    cimmited

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatapp-mern-socket-io-jwt.onrender.com", {
         query: {
           userId: authUser._id,
         },
